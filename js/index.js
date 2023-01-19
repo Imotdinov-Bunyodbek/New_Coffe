4031071bae5ae5e2a3da2fc9d1b21e89548c85db
Imotdinov-Bunyodbek <imotdinovbunyodbekk@gmail.com>
2023-01-19 14:08:38 +0500

    add js

---
+++ js/index.js
@@ -0,0 +1,4 @@
+var elBurgerBtn = document.querySelector(".js-open-btn");
+elBurgerBtn.addEventListener("click", function () {
+    elBurgerBtn.closest(".site-header").classList.toggle("sitenav-open")
+})
